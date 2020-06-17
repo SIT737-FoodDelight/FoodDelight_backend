d27e5c59b819f5bc4ccc402c0557fe9b60f97744
yugandhar-dev <yugandharp20@gmail.com>
2020-06-17 20:50:30 +1000

    twilio change

--- routes/twilio.js
+++ routes/twilio.js
@@ -7,7 +7,7 @@ router.post("/", jwtauth, async (req, res) => {
   var twiml = new twilio.twiml.MessagingResponse();
   twiml.message("I want to accept the order. Let's talk");
   res.writeHead(200, { "Content-Type": "text/xml" });
-  res.json("message sent");
+  res.end(twiml.toString());
 });
 
 module.exports = router;
