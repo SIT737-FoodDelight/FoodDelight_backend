e93d64d56e6e161fe069c22a07f3784a515500ee
Abhigna2202 <aparuvella@deakin.edu.au>
2020-06-17 19:35:37 +1000

    change showorders to post request

--- routes/showorders.js
+++ routes/showorders.js
@@ -3,7 +3,7 @@ const jwtAuth = require("../config/jwtAuth");
 const route = require("express").Router();
 const orders = require("../models/Order");
 
-route.get("/", jwtAuth, async (req, res) => {
+route.post("/", jwtAuth, async (req, res) => {
   orders.find({}, (err, orders) => {
     if (err) return res.json("error retrieving orders");
 
