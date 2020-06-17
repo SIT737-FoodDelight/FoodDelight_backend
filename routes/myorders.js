1f5a783c3afeb8b097c801d248a3227756a3472c
yugandhar-dev <yugandharp20@gmail.com>
2020-06-17 13:37:49 +1000

    myorders

--- routes/myorders.js
+++ routes/myorders.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const jwtauth = require("../config/jwtAuth");
+const User = require("../models/User");
+const Order = require("../models/Order");
+
+router.post("/", jwtauth, async (req, res) => {
+  const myOrders = await Order.find({
+    user_id: req.user._id,
+    order_status: true,
+  });
+  if (myOrders.length == 0) return res.json("your orders are not accepted");
+  res.json(myOrders);
+});
 
 module.exports = router;
